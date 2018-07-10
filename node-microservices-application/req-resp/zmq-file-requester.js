b4a2d7deb76974ca340b45793d852f1728164a83
rahiakela <rahi.akela@yahoo.com>
2018-07-10 15:06:56 +0530

    it is working fine.

--- node-microservices-application/req-resp/zmq-file-requester.js
+++ node-microservices-application/req-resp/zmq-file-requester.js
@@ -12,8 +12,10 @@ requester.on('message', data => {
   console.log('Received response:', response)
 })
 
-requester.connect('tcp://127.0.01:60401')
+requester.connect('tcp://127.0.0.1:60401')
 
-// Send a request for content.
-console.log(`Sending a request for ${filename} `)
-requester.send(JSON.stringify({ path: filename }))
+for (let i = 1; i <= 5; i++) {
+  // Send a request for content.
+  console.log(`Sending a request for ${filename} `)
+  requester.send(JSON.stringify({ path: filename }))
+}
